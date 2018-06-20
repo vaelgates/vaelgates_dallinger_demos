c7044e284baaeac6c7f9d2803ffee8711c6c9405
Jesse Snyder <jsnyder@wesleyan.edu>
2018-06-20 11:24:30 -0700

    Restructure code and de-lint

--- memoryexpt2/static/scripts/experiment.js
+++ memoryexpt2/static/scripts/experiment.js
@@ -1,194 +1,208 @@
-var uniqueWords = [];
-var currentNodeId;
+/*global $, dallinger, store */
 
-$(document).ready(function() {
-  // Print the consent form.
-  $("#print-consent").click(function() {
-    window.print();
-  });
+(function ($, dallinger, store) {
 
-  // Consent to the experiment.
-  $("#consent").click(function() {
-    store.set("hit_id", dallinger.getUrlParameter("hit_id"));
-    store.set("worker_id", dallinger.getUrlParameter("worker_id"));
-    store.set("assignment_id", dallinger.getUrlParameter("assignment_id"));
-    store.set("mode", dallinger.getUrlParameter("mode"));
+    var uniqueWords = [];
+    var currentNodeId;
+    // var FILLER_TASK_DURATION_MSECS = 30000
+    // var WORD_DISPLAY_DURATION_MSECS = 2000
+    // var FETCH_TRANSMISSION_FREQUENCY_MSECS = 100
+    var FILLER_TASK_DURATION_MSECS = 3000;
+    var WORD_DISPLAY_DURATION_MSECS = 1000;
+    var FETCH_TRANSMISSION_FREQUENCY_MSECS = 100;
 
-    dallinger.allowExit();
-    dallinger.goToPage('instructions');
-  });
+    $(document).ready(function() {
+        // Print the consent form.
+        $("#print-consent").click(function() {
+            window.print();
+        });
 
-  // Do not consent to the experiment.
-  $("#no-consent").click(function() {
-    dallinger.allowExit();
-    self.close();
-  });
+        // Consent to the experiment.
+        $("#consent").click(function() {
+            store.set("hit_id", dallinger.getUrlParameter("hit_id"));
+            store.set("worker_id", dallinger.getUrlParameter("worker_id"));
+            store.set("assignment_id", dallinger.getUrlParameter("assignment_id"));
+            store.set("mode", dallinger.getUrlParameter("mode"));
 
-  // Proceed to the waiting room.
-  $("#go-to-waiting-room").click(function() {
-    dallinger.allowExit();
-    dallinger.goToPage('waiting');
-  });
+            dallinger.allowExit();
+            dallinger.goToPage("instructions");
+        });
 
-  // Send a message.
-  $("#send-message").click(function() {
-    send_message();
-  });
+        // Do not consent to the experiment.
+        $("#no-consent").click(function() {
+            dallinger.allowExit();
+            self.close();
+        });
 
-  // Leave the chatroom.
-  $("#leave-chat").click(function() {
-    leave_chatroom();
-  });
+        // Proceed to the waiting room.
+        $("#go-to-waiting-room").click(function() {
+            dallinger.allowExit();
+            dallinger.goToPage("waiting");
+        });
 
-});
+        // Send a message.
+        $("#send-message").click(function() {
+            send_message();
+        });
 
-// Create the agent.
-create_agent = function() {
-  var deferred = dallinger.createAgent();
-  deferred.then(function (resp) {
-    currentNodeId = resp.node.id;
-    getWordList();
-  }, function (err) {
-    console.log(err);
-    errorResponse = JSON.parse(err.response);
-    if (errorResponse.hasOwnProperty("html")) {
-      $("body").html(errorResponse.html);
-    } else {
-      dallinger.allowExit();
-      dallinger.goToPage("questionnaire");
+        // Leave the chatroom.
+        $("#leave-chat").click(function() {
+            leave_chatroom();
+        });
+
+        if ($("body#experiment").length > 0) {
+            console.log("OK to create agent.");
+            create_agent();
+        }
+    });
+
+    // Create the agent.
+    function create_agent() {
+        var deferred = dallinger.createAgent();
+        deferred.then(function (resp) {
+            currentNodeId = resp.node.id;
+            getWordList();
+        }, function (rejection) {
+            console.log(rejection);
+            if (rejection.html) {
+                $("body").html(rejection.html);
+            } else {
+                dallinger.allowExit();
+                dallinger.goToPage("questionnaire");
+            }
+        });
     }
-  });
-};
 
 
-getWordList = function() {
-  dallinger.get("/node/" + currentNodeId + "/received_infos").done(
-    function(resp) {
-      var wordList = JSON.parse(resp.infos[0].contents);
-      showWordList(wordList);
-    },
-  );
-};
-
-showWordList = function(wl) {
-  if (wl.length === 0) {
-    // Show filler task.
-    showFillerTask();
-  } else {
-    // Show the next word.
-    $("#wordlist").html(wl.pop());
-    setTimeout(
-      function() {
-        showWordList(wl);
-      },
-      2000
-    );
-  }
-};
-
-showFillerTask = function() {
-  $("#stimulus").hide();
-  $("#fillertask-form").show();
-  var filler_answers = []
-
-  setTimeout(
-    function() {
-      // store results of filler tasks in array
-      $('#fillertask-form input').each( function( i, item ) {
-        filler_answers.push('{' + item.name + ': ' + item.value + '}');
-      });
-      // stores all filler answers in the contents column of info table
-      dallinger.createInfo(
-        currentNodeId,
-        {contents: filler_answers.join(", "), info_type: "Info"}
-      ).done(function(resp) {
-        showExperiment();
-      });
-
-    },
-    30000
-  );
-};
-
-showExperiment = function() {
-  $("#fillertask-form").hide();
-  $("#response-form").show();
-  $("#send-message").removeClass("disabled");
-  $("#send-message").html("Send");
-  $("#reproduction").focus();
-//  get_transmissions();
-};
-
-get_transmissions = function() {
-  dallinger.getTransmissions(
-    currentNodeId,
-    {status: "pending"}
-  ).done(function(resp) {
-    transmissions = resp.transmissions;
-    for (var i = transmissions.length - 1; i >= 0; i--) {
-      displayInfo(transmissions[i].info_id);
+    function getWordList() {
+        dallinger.get("/node/" + currentNodeId + "/received_infos").done(
+            function(resp) {
+                var wordList = JSON.parse(resp.infos[0].contents);
+                showWordList(wordList);
+            }
+        );
     }
-    setTimeout(function () { get_transmissions(currentNodeId); }, 100);
-  });
-};
 
-displayInfo = function(infoId) {
-  dallinger.getInfo(currentNodeId, infoId).done(
-    function(resp) {
-      var word = resp.info.contents.toLowerCase();
-      // if word hasn't appeared before, load into unique array and display
-      if (uniqueWords.indexOf(word) === -1) {
-        uniqueWords.push(word);
-        $("#reply").append("<p>" + word + "</p>");
-      }
-    },
-  );
-};
+    function showWordList(wl) {
+        if (wl.length === 0) {
+            // Show filler task.
+            showFillerTask();
+        } else {
+            // Show the next word.
+            $("#wordlist").html(wl.pop());
+            setTimeout(
+                function() {
+                    showWordList(wl);
+                },
+                WORD_DISPLAY_DURATION_MSECS
+            );
+        }
+    }
 
-send_message = function() {
-  response = $("#reproduction").val();
-  // typing box
-  // don't let people submit an empty response
-  if (response.length === 0) {
-    return;
-  }
+    function showFillerTask() {
+        var filler_answers = [];
+        $("#stimulus").hide();
+        $("#fillertask-form").show();
 
-  // let people submit only if word doesn't have a space
-  if (response.indexOf(" ") >= 0) {
-    return;
-  }
+        setTimeout(
+            function() {
+                // store results of filler tasks in array
+                $("#fillertask-form input").each(function( i, item ) {
+                    filler_answers.push("{" + item.name + ": " + item.value + "}");
+                });
+                // stores all filler answers in the contents column of info table
+                dallinger.createInfo(
+                    currentNodeId,
+                    {contents: filler_answers.join(", "), info_type: "Info"}
+                ).done(function(resp) {
+                    showExperiment();
+                });
+            },
+            FILLER_TASK_DURATION_MSECS
+        );
+    }
 
-  // will not let you add a word that is non-unique
-  if (uniqueWords.indexOf(response.toLowerCase()) === -1) {
-    uniqueWords.push(response.toLowerCase());
-    $(
-      "#reply"
-    ).append("<p style='color: #1693A5;'>" + response.toLowerCase() + "</p>"); //MONICA
-  } else {
-    return; // don't let people submit if non-unique
-  }
+    function showExperiment() {
+        $("#fillertask-form").hide();
+        $("#response-form").show();
+        $("#send-message").removeClass("disabled");
+        $("#send-message").html("Send");
+        $("#reproduction").focus();
+        get_transmissions();
+    }
 
-  $("#reproduction").val("");
-  $("#reproduction").focus();
+    function get_transmissions() {
+        dallinger.getTransmissions(
+            currentNodeId,
+            {status: "pending"}
+        ).done(function(resp) {
+            console.log("Got transmissions...");
+            var transmissions = resp.transmissions;
+            for (var i = transmissions.length - 1; i >= 0; i--) {
+                displayInfo(transmissions[i].info_id);
+            }
+            setTimeout(function () {
+                get_transmissions(currentNodeId);
+            },
+            FETCH_TRANSMISSION_FREQUENCY_MSECS);
+        });
+    }
 
-  dallinger.createInfo(
-    currentNodeId,
-    {contents: response, info_type: "Info"}
-  ).done(function(resp) {
-    $("#send-message").removeClass("disabled");
-    $("#send-message").html("Send");
-  });
-};
+    function displayInfo(infoId) {
+        dallinger.getInfo(currentNodeId, infoId
+        ).done(function(resp) {
+            var word = resp.info.contents.toLowerCase();
+            // if word hasn't appeared before, load into unique array and display
+            if (uniqueWords.indexOf(word) === -1) {
+                uniqueWords.push(word);
+                $("#reply").append("<p>" + word + "</p>");
+            }
+        });
+    }
 
+    function send_message() {
+        var response = $("#reproduction").val();
+        // typing box
+        // don't let people submit an empty response
+        if (response.length === 0) {
+            return;
+        }
 
-leave_chatroom = function() {
-  dallinger.allowExit();
-  dallinger.goToPage("questionnaire");
-};
+        // let people submit only if word doesn't have a space
+        if (response.indexOf(" ") >= 0) {
+            return;
+        }
 
-$(document).keypress(function(e) {
-  if (e.which === 13) {
-    $("#send-message").click();
-    return false;
-  }
-});
+        // will not let you add a word that is non-unique
+        if (uniqueWords.indexOf(response.toLowerCase()) === -1) {
+            uniqueWords.push(response.toLowerCase());
+            $("#reply").append("<p style='color: #1693A5;'>" + response.toLowerCase() + "</p>"); //MONICA
+        } else {
+            return; // don't let people submit if non-unique
+        }
+
+        $("#reproduction").val("");
+        $("#reproduction").focus();
+
+        dallinger.createInfo(
+            currentNodeId,
+            {contents: response, info_type: "Info"}
+        ).done(function(resp) {
+            $("#send-message").removeClass("disabled");
+            $("#send-message").html("Send");
+        });
+    }
+
+    function leave_chatroom() {
+        dallinger.allowExit();
+        dallinger.goToPage("questionnaire");
+    }
+
+    $(document).keypress(function(e) {
+        if (e.which === 13) {
+            $("#send-message").click();
+            return false;
+        }
+    });
+
+}($, dallinger, store));
