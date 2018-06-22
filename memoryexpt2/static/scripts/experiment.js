520619488ec0f66fc163fd0944b1a169886f212a
Jesse Snyder <jsnyder@wesleyan.edu>
2018-06-22 13:11:57 -0700

    Move socket to separate file

--- memoryexpt2/static/scripts/experiment.js
+++ memoryexpt2/static/scripts/experiment.js
@@ -1,6 +1,6 @@
-/*global $, dallinger, ReconnectingWebSocket */
+/*global $, dallinger, pubsub */
 
-(function ($, dallinger, ReconnectingWebSocket) {
+(function ($, dallinger, pubsub) {
 
     var currentNodeId;
     var currentPlayerId;
@@ -113,90 +113,6 @@
         return WordSubmission;
     }());
 
-    var ServerSocket = (function () {
-        var makeSocket = function (endpoint, channel, tolerance) {
-            var ws_scheme = (window.location.protocol === "https:") ? "wss://" : "ws://",
-                app_root = ws_scheme + location.host + "/",
-                socket;
-
-            socket = new ReconnectingWebSocket(
-                app_root + endpoint + "?channel=" + channel + "&tolerance=" + tolerance
-            );
-            socket.debug = true;
-
-            return socket;
-        };
-
-        var dispatch = function (self, event) {
-            var marker = self.broadcastChannel + ":";
-            if (event.data.indexOf(marker) !== 0) {
-                console.log(
-                    "Message was not on channel " + self.broadcastChannel + ". Ignoring.");
-                return;
-            }
-            var msg = JSON.parse(event.data.substring(marker.length));
-
-            var callback = self.callbackMap[msg.type];
-            if (typeof callback !== "undefined") {
-                callback(msg);
-            } else {
-                console.log("Unrecognized message type " + msg.type + " from backend.");
-            }
-        };
-
-        /*
-         * Public API
-         */
-        var Socket = function (settings) {
-            if (!(this instanceof Socket)) {
-                return new Socket(settings);
-            }
-
-            var self = this,
-                tolerance = typeof(settings.lagTolerance) !== "undefined" ? settings.lagTolerance : 0.1;
-
-            this.broadcastChannel = settings.broadcast;
-            this.controlChannel = settings.control;
-            this.callbackMap = settings.callbackMap;
-
-
-            this.socket = makeSocket(
-                settings.endpoint, this.broadcastChannel, tolerance);
-
-            this.socket.onmessage = function (event) {
-                dispatch(self, event);
-            };
-        };
-
-        Socket.prototype.open = function () {
-            var isOpen = $.Deferred();
-
-            this.socket.onopen = function (event) {
-                isOpen.resolve();
-            };
-
-            return isOpen;
-        };
-
-        Socket.prototype.send = function (data) {
-            var msg = JSON.stringify(data),
-                channel = this.controlChannel;
-
-            console.log("Sending message to the " + channel + " channel: " + msg);
-            this.socket.send(channel + ":" + msg);
-        };
-
-        Socket.prototype.broadcast = function (data) {
-            var msg = JSON.stringify(data),
-                channel = this.broadcastChannel;
-
-            console.log("Broadcasting message to the " + channel + " channel: " + msg);
-            this.socket.send(channel + ":" + msg);
-        };
-
-        return Socket;
-    }());
-
     $(document).ready(function() {
 
         var egoParticipantId = dallinger.getUrlParameter("participant_id"),
@@ -229,7 +145,7 @@
             "lagTolerance": 0.001,
             "callbackMap": callbackMap
         };
-        var socket = new ServerSocket(socketSettings);
+        var socket = pubsub.Socket(socketSettings);
 
         socket.open().done(
             function () {
@@ -341,4 +257,4 @@
         });
     }
 
-}($, dallinger, ReconnectingWebSocket));
+}($, dallinger, pubsub));
---
+++ memoryexpt2/static/scripts/pubsub.js
@@ -0,0 +1,95 @@
+/*global ReconnectingWebSocket */
+
+var pubsub = (function (ReconnectingWebSocket) {
+
+    var backend = {};
+
+    backend.Socket = (function () {
+        var makeSocket = function (endpoint, channel, tolerance) {
+            var ws_scheme = (window.location.protocol === "https:") ? "wss://" : "ws://",
+                app_root = ws_scheme + location.host + "/",
+                socket;
+
+            socket = new ReconnectingWebSocket(
+                app_root + endpoint + "?channel=" + channel + "&tolerance=" + tolerance
+            );
+            socket.debug = true;
+
+            return socket;
+        };
+
+        var dispatch = function (self, event) {
+            var marker = self.broadcastChannel + ":";
+            if (event.data.indexOf(marker) !== 0) {
+                console.log(
+                    "Message was not on channel " + self.broadcastChannel + ". Ignoring.");
+                return;
+            }
+            var msg = JSON.parse(event.data.substring(marker.length));
+
+            var callback = self.callbackMap[msg.type];
+            if (typeof callback !== "undefined") {
+                callback(msg);
+            } else {
+                console.log("Unrecognized message type " + msg.type + " from backend.");
+            }
+        };
+
+        /*
+         * Public API
+         */
+        var Socket = function (settings) {
+            if (!(this instanceof Socket)) {
+                return new Socket(settings);
+            }
+
+            var self = this,
+                tolerance = typeof(settings.lagTolerance) !== "undefined" ? settings.lagTolerance : 0.1;
+
+            this.broadcastChannel = settings.broadcast;
+            this.controlChannel = settings.control;
+            this.callbackMap = settings.callbackMap;
+
+
+            this.socket = makeSocket(
+                settings.endpoint, this.broadcastChannel, tolerance);
+
+            this.socket.onmessage = function (event) {
+                dispatch(self, event);
+            };
+        };
+
+        Socket.prototype.open = function () {
+            var isOpen = $.Deferred();
+
+            this.socket.onopen = function (event) {
+                isOpen.resolve();
+            };
+
+            return isOpen;
+        };
+
+        Socket.prototype.send = function (data) {
+            var msg = JSON.stringify(data),
+                channel = this.controlChannel;
+
+            console.log("Sending message to the " + channel + " channel: " + msg);
+            this.socket.send(channel + ":" + msg);
+        };
+
+        Socket.prototype.broadcast = function (data) {
+            var msg = JSON.stringify(data),
+                channel = this.broadcastChannel;
+
+            console.log("Broadcasting message to the " + channel + " channel: " + msg);
+            this.socket.send(channel + ":" + msg);
+        };
+
+        return Socket;
+    }());
+
+
+    return backend;
+
+
+}(ReconnectingWebSocket));
