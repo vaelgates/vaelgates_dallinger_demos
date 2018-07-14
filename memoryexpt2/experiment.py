78edaa9e9ed9756c8c6ac311926ebe3d54c70a20
Carlos de la Guardia <cguardia@yahoo.com>
2018-07-13 20:43:56 -0500

    pay bonuus nased on waiting time

--- memoryexpt2/experiment.py
+++ memoryexpt2/experiment.py
@@ -1,3 +1,4 @@
+import datetime
 import flask
 import gevent
 import json
@@ -56,6 +57,13 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         ]
 
     def handle_connect(self, msg):
+        DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+        end_waiting_room = datetime.datetime.now().strftime(DATETIME_FORMAT)
+        participant = self.session.query(dallinger.models.Participant)
+        participant.property1 = end_waiting_room
+        self.session.commit()
+        logger.info("Player {} waiting room exit: {}.".format(player_id,
+                                                              end_waiting_room))
         self.game.add_player(msg['player_id'])
 
     def handle_disconnect(self, msg):
@@ -159,7 +167,14 @@ class CoordinationChatroom(dlgr.experiments.Experiment):
         """Give the participant a bonus for waiting."""
 
         DOLLARS_PER_HOUR = 5.0
-        t = participant.end_time - participant.creation_time
+        DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+        try:
+            end_waiting_room = datetime.datetime.strptime(participant.property1,
+                                                          DATETIME_FORMAT)
+        except ValueError:
+            # just in case something went wrong saving wait room end time
+            end_waiting_room = participant.end_time
+        t = end_waiting_room - participant.creation_time
 
         # keep to two decimal points otherwise doesn't work
         return round((t.total_seconds() / 3600) * DOLLARS_PER_HOUR, 2)
