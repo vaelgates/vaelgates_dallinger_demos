ccc8249e6c9943871e418cf97e556fec8a8a5431
Vael Gates <vaelgates@gmail.com>
2019-12-20 22:59:58 -0800

    added new squares expt

---
+++ squares_Rknowsgoal/experiment.py
@@ -0,0 +1,39 @@
+from dallinger.experiment import Experiment
+
+
+class Squares(Experiment):
+    """Define the structure of the experiment."""
+
+    def __init__(self, session=None):
+        """Call the same function in the super (see experiments.py in dallinger).
+
+        The models module is imported here because it must be imported at
+        runtime.
+
+        A few properties are then overwritten.
+
+        Finally, setup() is called.
+        """
+        super(Squares, self).__init__(session)
+        self.experiment_repeats = 1
+        self.initial_recruitment_size = 2 #60
+        if session:
+            self.setup()
+
+    def recruit(self):
+        if not self.networks(full=False):
+            self.recruiter.close_recruitment()
+
+    def bonus(self, participant):
+        """Give the participant a bonus for optimally completing grids"""
+
+        # keep to two decimal points otherwise doesn't work
+        return .10 #participant.bonus_earned
+        # MONICA: this should be participant.bonus_earned from index.js, 
+        # but I don't know how to do this so I'm just giving everyone
+        # the full bonus right now.
+
+
+    # hack for existing bug in 4.0.0
+    def is_overrecruited(self, waiting_count):
+        return False
---
+++ squares_Rknowsgoal/static/scripts/experiment.js
@@ -0,0 +1,79 @@
+var my_node_id;
+
+// Consent to the experiment.
+$(document).ready(function() {
+
+  // do not allow user to close or reload
+  dallinger.preventExit = true;
+
+  // Print the consent form.
+  $("#print-consent").click(function() {
+    window.print();
+  });
+
+  // Consent to the experiment.
+  $("#consent").click(function() {
+    store.set("hit_id", dallinger.getUrlParameter("hit_id"));
+    store.set("worker_id", dallinger.getUrlParameter("worker_id"));
+    store.set("assignment_id", dallinger.getUrlParameter("assignment_id"));
+    store.set("mode", dallinger.getUrlParameter("mode"));
+
+    dallinger.allowExit();
+    window.location.href = '/instructions-1';
+  });
+
+  // Consent to the experiment.
+  $("#no-consent").click(function() {
+    dallinger.allowExit();
+    window.close();
+  });
+
+
+  // Go-to-experiment button-- only proceed if you get all questions correct
+  $("#go-to-experiment").click(function() {
+    paradigmQ = $("#paradigmQ").val()
+    knowgoalQ = $("#knowgoalQ").val()
+    diffpartnerQ = $("#diffpartnerQ").val()
+    if (paradigmQ == 1) {
+      if (knowgoalQ == 1){ 
+        if (diffpartnerQ == 1){
+          dallinger.allowExit();
+          dallinger.goToPage('experiment');
+        }
+      }
+    }
+  });
+
+  // Finish game
+  $("#alldone").click(function() {
+    $("#alldone").addClass('disabled');
+    $("#alldone").html('Sending...');
+    dallinger.allowExit();
+    dallinger.goToPage('questionnaire');
+  });
+
+});
+
+// Create the agent.
+var create_agent = function() {
+  dallinger.createAgent()
+    .done(function (resp) {
+      my_node_id = resp.node.id;
+      get_info();
+    })
+    .fail(function () {
+      dallinger.allowExit();
+      dallinger.goToPage('questionnaire');
+    });
+};
+
+var get_info = function() {
+  dallinger.getReceivedInfos(my_node_id)
+    .done(function (resp) {
+    })
+    .fail(function (err) {
+      console.log(err);
+      var errorResponse = JSON.parse(err.response);
+      $('body').html(errorResponse.html);
+    });
+};
