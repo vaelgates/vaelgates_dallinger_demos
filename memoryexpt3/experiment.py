479bd876c96e3192816b4bf19bc74a3e18169791
Vael Gates <vaelgates@gmail.com>
2021-04-01 16:24:23 -0700

    updates to the experiment

--- memoryexpt3/experiment.py
+++ memoryexpt3/experiment.py
@@ -67,7 +67,7 @@ class CoordinationChatroom(Experiment):
         super(CoordinationChatroom, self).__init__(session)
         config = dlgr.config.get_config()
         self.experiment_repeats = 1
-        self.num_participants = 2 #55 #55 #140 below
+        self.num_participants = 4 #55 #55 #140 below
         self.initial_recruitment_size = self.num_participants *2 # *3 #note: can't do *2.5 here, won't run even if the end result is an integer
         self.quorum = self.num_participants  # quorum is read by waiting room
         self.words_aloud = config.get(u'mexp_words_aloud', False)
